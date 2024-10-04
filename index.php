8c50f0260964f392334f7e503da1acdd10e345a1
Timon Kisera <timonmarick@gmail.com>
2024-10-04 21:50:29 +0300

    Creating objects and decomposing index file

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup Form</title>
-</head>
-<body>
-    <!--Verifying Connection-->
-    <?php
-        // Fetching connection file
-        require_once ('Components/connection.php');
+<?php
+    // Reading objects file
+    require_once ('loadfile.php');
 
-        // Validating connection
-        if (isset($conn) && $conn)
-        {
-            echo "<h1>Connection Achieved</h1>";
-        }
-        else
-        {
-            echo "<h1>Connection Failed</h1>";
-        }
-    ?>
-</body>
-</html>
+    $signup->signUpHeader();
+    $signup->signUpForm();
+    $footer->footer();
---
+++ loadfile.php
@@ -0,0 +1,15 @@
+<?php
+
+// Reading classes
+require 'Structures/Homepage/home.php';
+require 'Structures/Footer/footer.php';
+require 'Structures/Forms/signup.php';
+
+//Homepage instances
+$homepage = new home();
+
+//Footer instances
+$footer = new footer();
+
+// Signup form instances
+$signup = new signupform();

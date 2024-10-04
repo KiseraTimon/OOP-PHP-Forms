507d09c840e2eba37c6180ffb262217350e0d0d3
Timon Kisera <timonmarick@gmail.com>
2024-10-04 15:50:34 +0000

    Simple Bootstrap form template

--- index.php
+++ index.php
@@ -3,9 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup</title>
+    <title>Signup Form</title>
 </head>
 <body>
-    
+    <!--Bootstrap Form-->
+    <form>
+        <!--First name and Last name-->
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="First name">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Last name">
+            </div>
+        </div>
+
+        <!--Username-->
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Username">
+            </div>
+        </div>
+
+        <!--Email-->
+        <div class="form-row">
+            <div class="col">
+                <input type="email" class="form-control" placeholder="Email">
+            </div>
+        </div>
+
+        <!--Password-->
+        <div class="form-row">
+            <div class="col">
+                <input type="password" class="form-control" placeholder="Password">
+            </div>
+        </div>
+
+        <!--Confirm Password-->
+        <div class="form-row">
+            <div class="col">
+                <input type="password" class="form-control" placeholder="Confirm Password">
+            </div>
+        </div>
+    </form>
 </body>
 </html>

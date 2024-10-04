a1cc65e4634c753dd407f3d1494ff7989b60aec3
Timon Kisera <timonmarick@gmail.com>
2024-10-04 19:54:17 +0300

    Revert "Checking Database Connection from index.php"
    This reverts commit 454deee8ceb262c9f179bc7816ce7ffb9c037265.

--- index.php
+++ index.php
@@ -6,16 +6,45 @@
     <title>Signup Form</title>
 </head>
 <body>
-    <!--Testing Database Connection-->
-    <?php
-        // Including PHP PDO connection
-        require_once('Components/connection.php');
+    <!--Bootstrap Form-->
+    <form>
+        <!--First name and Last name-->
+        <div class="form-row">
+            <div class="col">
+                <input type="text" id="fname" class="form-control" placeholder="First name">
+            </div>
+            <div class="col">
+                <input type="text" id="lname" class="form-control" placeholder="Last name">
+            </div>
+        </div>
 
-        // Validating Connection
-        if($conn)
-        {
-            echo "<h1>Database Connection Successful</h1>";
-        }
-    ?>
+        <!--Username-->
+        <div class="form-row">
+            <div class="col">
+                <input type="text" id="uname" class="form-control" placeholder="Username">
+            </div>
+        </div>
+
+        <!--Email-->
+        <div class="form-row">
+            <div class="col">
+                <input type="email" id="email" class="form-control" placeholder="Email">
+            </div>
+        </div>
+
+        <!--Password-->
+        <div class="form-row">
+            <div class="col">
+                <input type="password" id="password" class="form-control" placeholder="Password">
+            </div>
+        </div>
+
+        <!--Confirm Password-->
+        <div class="form-row">
+            <div class="col">
+                <input type="password" id="confirmpassword" class="form-control" placeholder="Confirm Password">
+            </div>
+        </div>
+    </form>
 </body>
 </html>
